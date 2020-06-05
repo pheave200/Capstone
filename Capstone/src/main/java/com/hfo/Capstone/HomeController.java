7f26f0b1b8668e1f3f3afc9352c173f3d0568d2f
pheave200 <phil_heavey@cable.comcast.com>
2020-06-05 10:23:25 -0400

    add HomeController & index

---
+++ Capstone/src/main/java/com/hfo/Capstone/HomeController.java
@@ -0,0 +1,16 @@
+package com.hfo.Capstone;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("")
+    public String index(Model model) {
+        model.addAttribute("title", "Family Organizer");
+
+        return "index";
+    }
+}
