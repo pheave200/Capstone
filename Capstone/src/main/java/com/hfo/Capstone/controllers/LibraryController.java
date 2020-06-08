a936d0ebb71dd0e9317a2e0e71748d1bc5d9f729
pheave200 <phil_heavey@cable.comcast.com>
2020-06-08 15:31:59 -0400

    library index & add

---
+++ Capstone/src/main/java/com/hfo/Capstone/controllers/LibraryController.java
@@ -0,0 +1,45 @@
+package com.hfo.Capstone.controllers;
+
+import com.hfo.Capstone.models.Book;
+import com.hfo.Capstone.models.AbstractEntity;
+import com.hfo.Capstone.models.data.LibraryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("library")
+public class LibraryController extends AbstractEntity {
+
+    @Autowired
+    LibraryRepository libraryRepository;
+
+    @RequestMapping("")
+    public String index(Model model) {
+        model.addAttribute("title", "Home Library");
+        model.addAttribute("books", libraryRepository.findAll());
+        return "library/index";
+    }
+
+    @GetMapping("add")
+    public String displayAddBookForm(Model model) {
+        model.addAttribute(new Book());
+        return "library/add";
+    }
+
+    @PostMapping("add")
+    public String processAddBookForm(@ModelAttribute @Valid Book newBook, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            return "library/add";
+        }
+        libraryRepository.save(newBook);
+        return "library/index";
+    }
+}
--- Capstone/src/main/java/com/hfo/Capstone/models/Book.java
+++ Capstone/src/main/java/com/hfo/Capstone/models/Book.java
@@ -6,5 +6,31 @@ import javax.persistence.Entity;
 @Entity
 public class Book extends AbstractEntity{
 
+    private String author;
 
+    private String genre;
+
+    public Book() {}
+
+    public Book(String author, String genre) {
+        super();
+        this.author=author;
+        this.genre=genre;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
